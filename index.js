6c917f8528c87ab1405e9f930282c08773a69673
Redouane Bouchmaa <red.bouchmaa.dev@gmail.com>
2024-02-05 17:02:15 +0100

    add scrollUp arrow / responsive min width 1500px

--- index.js
+++ index.js
@@ -32,3 +32,18 @@ menuLinks.forEach(link => {
     link.classList.add('active');
   });
 });
+
+
+  /******************* SHOW SCROLL UP ************************/ 
+  const arrowUp = () =>{
+      // on récupère l'élément arrowUp
+      const showScrollUpBtn = document.getElementById("arrowUp");
+      // si le déilement est sup ou = à 350 on affiche arrowUp 
+      if(window.scrollY >= 350){
+      showScrollUpBtn.classList.add('showArrowUp');
+      }else{
+          showScrollUpBtn.classList.remove('showArrowUp');
+          }
+  }
+  window.addEventListener('scroll', arrowUp);
+
